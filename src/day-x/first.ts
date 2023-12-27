1d4258200fdf8fdd180e6e1effaea86b6be99046
Sebastian Kriesche <sebastian.kriesche@up2parts.com>
2023-12-27 08:57:07 +0100

    feat: cleanup

--- src/day-x/first.ts
+++ src/day-x/first.ts
@@ -1,4 +1,7 @@
-import { input } from './input'
+import { readFileSync } from 'fs'
+import path from 'path'
+
+const input = readFileSync(path.join(__dirname, '../../src/day-14', 'input.txt'), 'utf-8').split('\n')
 
 export function first() {
   console.log(input)
